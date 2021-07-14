fcaafcaaea89be62b730c5454176ce5ac33a8380
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2021-07-15 01:10:59 +0530

    Vaibhav Bank✔

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function sendMoney(){
+   var enterName = document.getElementById("enterName").value;
+   var enterAmount = parseInt(document.getElementById("enterAmount").value);
+var enterSName = document.getElementById("enterSName").value;
+ var findSenderBankAccount = enterSName + "BankBalance";
+   var enterSAmount =parseInt(document.getElementById(findSenderBankAccount).innerHTML);
+
+
+   if (enterAmount > enterSAmount) {
+      alert("Insufficient Balance.")
+   }
+
+else {
+      var findUserBankAccount = enterName + "BankBalance";
+
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById(findSenderBankAccount).innerHTML) - enterAmount;
+       document.getElementById(findSenderBankAccount).innerHTML=myAccountBalance ;
+      document.getElementById(findUserBankAccount).innerHTML=finalAmount  ;
+      alert(`Successful Transaction !!
+      $${enterAmount} is sent to ${enterName}@email.com.`)
+
+
+   }
+}
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
